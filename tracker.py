1a9e221481e2df84da6f1d097e071e0fefecb730
zhangdeniel11 <denielzhang2007@gmail.com>
2026-03-06 23:54:04 +0000

    Tracker

--- tracker.py
+++ tracker.py
@@ -2,3 +2,13 @@ import data
 
 def add_session():
     subject = input("enter subject/course: ")
+    minutes = int(input("Enter amount of minutes studied: "))
+    location = input("Enter the location: ")
+    notes = input("enter your notes (optional): ")
+
+    data.subjects.append(subject)
+    data.minutes.append(minutes)
+    data.locations.append(location)
+    data.notes.append(notes)
+
+    print("Session added!")
